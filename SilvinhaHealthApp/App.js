0c586f4e63bb9ba8d27b6185baa8861028d714dd
CapivaraCar1oca <vitorhenriquemartins05@gmail.com>
2025-03-12 13:22:17 -0300

    Desfazendo a merda

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -1,34 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Silvinha Health App</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.titlecontainer}>
+       <Text style={styles.title}>Silvinha Health App</Text>
       </View>
-      <View style={styles.formContent}>
+      <View style={styles.content}>
+       <Text style={styles.subtitle}>Calculadora de IMC</Text>
+
+        <View>
+          <Text style={styles.label}>Altura</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='Ex.'
+            keyboardType='numeric'
+          ></TextInput>
+        </View>
 
       </View>
-
-    </View>
+      <StatusBar style='light' />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e0e5e5',
-    paddingTop: 50,
+    backgroundColor: '#EDF2F4',
+
   },
-  titleContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding:10,
+  titlecontainer: {
+    alignItems:'center',
+    justifyContent: 'flex-end',
+    height: 130,
+    backgroundColor: '#D90429',
+    borderBottomStartRadius: 25,
+    borderBottomEndRadius: 25,
   },
-  title: {
-    color: "#ff0043",
+  title:{
+    color:'#EDF2F4',
     fontSize: 28,
-    fontWeight: 'bold'
+    fontWeight:'bold',
+    marginBottom: 30,
+  },
+  content:{
+    flex: 1,
+    padding: 40,
+    widht:'100%',
+    backgroundColor:'EDF2F4'
+    
+  },
+  subtitle:{
+    textAlign: 'center',
+    fontSize: 24,
+    color:'D90429',
+    fontWeight:'bold',
+    marginBottom: 40,
+  },
+  label:{
+    color: '#000',
+    fontSize: 18,
+  },
+  Input:{
+    height: 45,
+    widht:'100%',
+    fontSize: 18,
+    borderColor: 'D90429',
+    borderBottomWidht: 1
   }
 });
